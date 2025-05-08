de2989b6cc394b60d8a108e56096fc7f17bf2c77
ashishjuris <ashish.juris@gmail.com>
2025-05-07 23:32:58 -0700

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>  
+    <p className='m-10 bg-black text-white '> 
+  Click on the Vite and React logos to learn more
+  </p> 
+    </>
+  )
+}
+
+export default App
